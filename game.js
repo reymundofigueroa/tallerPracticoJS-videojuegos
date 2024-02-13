615434f7be39d2977a0905351c9d4548a52877f9
crdCalma <cdguttierrez11@gmail.com>
2024-02-13 05:54:37 -0500

    primeros pasos

---
+++ game.js
@@ -0,0 +1,19 @@
+const canvas = document.querySelector('#game')
+const game = canvas.getContext('2d')
+
+window.addEventListener('load', startGame)
+
+function startGame() {
+game.fillRect(0,0,100,100)
+}
+
+
+
+
+
+
+//NOVEDADES
+//getContext
+//fillRect()
+//clearRect()
+//fillText
