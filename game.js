9bbafe90b950ea230f03827691a27ed57b023da3
crdCalma <cdguttierrez11@gmail.com>
2024-02-14 06:07:01 -0500

    Tamaño del canvas

--- game.js
+++ game.js
@@ -4,7 +4,24 @@ const game = canvas.getContext('2d')
 window.addEventListener('load', startGame)
 
 function startGame() {
-game.fillRect(0,0,100,100)
+    let canvasSize
+
+    if (window.innerHeight > window.innerWidth) {
+        canvasSize = window.innerWidth * 0.8
+    } else {
+        canvasSize = window.innerHeight * 0.8
+    } 
+
+    canvas.setAttribute('width', canvasSize)
+    canvas.setAttribute('height', canvasSize)
+
+    let elementsSize = canvasSize / 10
+
+    game.font = elementsSize + 'px Verdana' //siempre debe ir antes del fillText
+    game.textAlign = 'end'
+    for (let i = 1; i <= 10; i++) {
+        game.fillText(emojis['X'], elementsSize * i, elementsSize)
+    }
 }
 
 
@@ -12,8 +29,3 @@ game.fillRect(0,0,100,100)
 
 
 
-//NOVEDADES
-//getContext
-//fillRect()
-//clearRect()
-//fillText
